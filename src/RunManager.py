09e376f3fb9b36e8e2e856484526e39f16c1e2bc
David Masad <david.masad@gmail.com>
2013-06-13 17:48:35 -0600

    Tweaked tabs

--- src/RunManager.py
+++ src/RunManager.py
@@ -24,11 +24,11 @@ class RunManager(object):
         self.attacker = attacker
         self.defender = defender
         
-	self.nodes_lost_edges = []
-
-	self.num_iterations = num_iterations
+        self.nodes_lost_edges = []
+    
+        self.num_iterations = num_iterations
         self.G = nx.Graph()
-	G.add_nodes_from(range(nmb_nodes))
+        self.G.add_nodes_from(range(nmb_nodes))
         self.fitness = fitness
         self.fitness_per_round = []
         
@@ -36,12 +36,11 @@ class RunManager(object):
         '''
         Run the run to completion, and return the network fitness series.
         '''
-        
-        self.build_initial_network(initialEdges)
-	new_fitness = self.fitness(self.G)
+        self.build_initial_network(self.initialEdges)
+        new_fitness = self.fitness(self.G)
         self.fitness_per_round.append(new_fitness)
         for round in range(self.num_iterations):
-	    self.calculate_network_metrics()
+            self.calculate_network_metrics()
             self.attack_network()
             self.defend_network()
             new_fitness = self.fitness(self.G)
@@ -49,38 +48,30 @@ class RunManager(object):
         return self.fitness_per_round
         
     def build_initial_network(self,initialEdges):
-	for i in range(initialEdges):
-		self.defend_network()        
-	pass
+        for i in range(initialEdges):
+            self.defend_network()        
     
     def attack_network(self):
-
-	attacked_node=self.attacker.which_node_to_attack()  
-	self.nodes_lost_edges=self.G.neighbors(attacked_node)
-	self.G.remove_node(attacked_node)	
-
-	pass
+        attacked_node=self.attacker.which_node_to_attack()  
+        self.nodes_lost_edges=self.G.neighbors(attacked_node)
+        self.G.remove_node(attacked_node)	
 
     
     def defend_network(self):
-	edges_to_rewire=self.defender.defender_network(self.nodes_lost_edges)
-	'''
-	edges to rewire are contained within edges_to_rewire
-	'''
-	self.G.add_edges_from(edges_to_rewire)
-	
-        pass
+        edges_to_rewire=self.defender.defender_network(self.nodes_lost_edges)
+        #edges to rewire are contained within edges_to_rewire
 
-    def calculate_network_metrics_attacker():
-	'''
+        self.G.add_edges_from(edges_to_rewire)
+
+    def calculate_network_metrics_attacker(self):
+        '''
         calculate the network metrics used by the attacker
         for all nodes in the network
         returns a matrix of nodes*metrics
         '''
-   	
-	return []
+        return []
     
-    def calculate_network_metrics_defender():
+    def calculate_network_metrics_defender(self):
         '''
         calculate the network metrics used by the defender
         for all nodes in the network
