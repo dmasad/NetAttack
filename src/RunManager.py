d431c2752730df5133c800c3cc00972d59961482
joph <johannes.schmidt@boku.ac.at>
2013-06-13 14:09:17 -0600

    src/RunManager1.py

--- src/RunManager.py
+++ src/RunManager.py
@@ -33,20 +33,45 @@ class RunManager(object):
         Run the run to completion, and return the network fitness series.
         '''
         
-        self.build_initial_network()
+        self.build_initial_network(initialEdges)
+	new_fitness = self.fitness(self.G)
+        self.fitness_per_round.append(new_fitness)
         for round in range(self.num_iterations):
+	    self.calculate_network_metrics()
             self.attack_network()
-            self.rebuild_network()
+            self.defend_network()
             new_fitness = self.fitness(self.G)
             self.fitness_per_round.append(new_fitness)
         return self.fitness_per_round
         
-    def build_initial_network(self):
-        pass
+    def build_initial_network(self,initialEdges):
+	for i in range(initialEdges):
+		self.defend_network()        
+	pass
     
     def attack_network(self):
-        pass
+	attacker.attack_network()        
+	pass
     
-    def rebuild_network(self):
+    def defend_network(self):
+	defender.defender_network()
         pass
+
+    def calculate_network_metrics_attacker():
+	'''
+        calculate the network metrics used by the attacker
+        for all nodes in the network
+        returns a matrix of nodes*metrics
+        '''
+   
+	pass
     
+    def calculate_network_metrics_defender():
+        '''
+        calculate the network metrics used by the defender
+        for all nodes in the network
+        returns a matrix of nodes*metrics
+        '''
+        return []
+        pass
+        
